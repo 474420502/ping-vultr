80d7fc442ad2fb5d384c3169fdc2c881f75f4677
eson <474420502@qq.com>
2021-03-11 10:17:38 +0800

    feat(shell): finish ping vultr shell

---
+++ ping-vultr.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "(Asia)Seoul, Korea[韩国 首尔]	141.164.34.61" && ping -c 10	sel-kor-ping.vultr.com
+echo "(Asia)Tokyo, Japan[日本 东京]	108.61.201.151" && ping -c 10	hnd-jp-ping.vultr.com
+echo "Singapore[新加坡]	45.32.100.168" && ping -c 10	sgp-ping.vultr.com
+echo "(AU) Sydney, Australia[悉尼]	108.61.212.117" && ping -c 10	syd-au-ping.vultr.com
+echo "(EU) Frankfurt, DE[德国 法兰克福]	108.61.210.117" && ping -c 10	fra-de-ping.vultr.com
+echo "(EU) Amsterdam, NL[荷兰 阿姆斯特丹]	108.61.198.102" && ping -c 10	ams-nl-ping.vultr.com
+echo "(EU) London, UK[英国 伦敦]	108.61.196.101" && ping -c 10	lon-gb-ping.vultr.com
+echo "(EU) Paris, France[法国 巴黎]	108.61.209.127" && ping -c 10	par-fr-ping.vultr.com
+echo "Seattle, Washington[美东 华盛顿州 西雅图]	108.61.194.105" && ping -c 10	wa-us-ping.vultr.com
+echo "Silicon Valley, Ca[美西 加州 硅谷]	104.156.230.107" && ping -c 10	sjo-ca-us-ping.vultr.com
+echo "Los Angeles, Ca[美西 加州 洛杉矶]	108.61.219.200" && ping -c 10	lax-ca-us-ping.vultr.com
+echo "Chicago, Illinois[美东 芝加哥]	107.191.51.12" && ping -c 10	il-us-ping.vultr.com
+echo "Dallas, Texas[美中 德克萨斯州 达拉斯]	108.61.224.175" && ping -c 10	tx-us-ping.vultr.com
+echo "New York / New Jersey[美东 新泽西]	108.61.149.182" && ping -c 10	nj-us-ping.vultr.com
+echo "Atlanta, Georgiaa[美东 乔治亚州 亚特兰大]	108.61.193.166" && ping -c 10	ga-us-ping.vultr.com
+echo "Miami, Florida[美东 佛罗里达州 迈阿密]	104.156.244.232" && ping -c 10	fl-us-ping.vultr.com
+echo "Toronto, Canada[加拿大 多伦多]	149.248.50.81" && ping -c 10	tor-ca-ping.vultr.com
