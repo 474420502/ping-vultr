37b45c266ef758c7a3eeca219525d9a06b80457b
eson <474420502@qq.com>
2023-05-14 22:45:28 +0800

    update

--- ping-vultr.sh
+++ ping-vultr.sh
@@ -1,18 +1,27 @@
 #! /bin/bash
-echo "(Asia)Seoul, Korea[韩国 首尔]	141.164.34.61" && ping -c 10	sel-kor-ping.vultr.com
-echo "(Asia)Tokyo, Japan[日本 东京]	108.61.201.151" && ping -c 10	hnd-jp-ping.vultr.com
-echo "Singapore[新加坡]	45.32.100.168" && ping -c 10	sgp-ping.vultr.com
-echo "(AU) Sydney, Australia[悉尼]	108.61.212.117" && ping -c 10	syd-au-ping.vultr.com
-echo "(EU) Frankfurt, DE[德国 法兰克福]	108.61.210.117" && ping -c 10	fra-de-ping.vultr.com
-echo "(EU) Amsterdam, NL[荷兰 阿姆斯特丹]	108.61.198.102" && ping -c 10	ams-nl-ping.vultr.com
-echo "(EU) London, UK[英国 伦敦]	108.61.196.101" && ping -c 10	lon-gb-ping.vultr.com
-echo "(EU) Paris, France[法国 巴黎]	108.61.209.127" && ping -c 10	par-fr-ping.vultr.com
-echo "Seattle, Washington[美东 华盛顿州 西雅图]	108.61.194.105" && ping -c 10	wa-us-ping.vultr.com
-echo "Silicon Valley, Ca[美西 加州 硅谷]	104.156.230.107" && ping -c 10	sjo-ca-us-ping.vultr.com
-echo "Los Angeles, Ca[美西 加州 洛杉矶]	108.61.219.200" && ping -c 10	lax-ca-us-ping.vultr.com
-echo "Chicago, Illinois[美东 芝加哥]	107.191.51.12" && ping -c 10	il-us-ping.vultr.com
-echo "Dallas, Texas[美中 德克萨斯州 达拉斯]	108.61.224.175" && ping -c 10	tx-us-ping.vultr.com
-echo "New York / New Jersey[美东 新泽西]	108.61.149.182" && ping -c 10	nj-us-ping.vultr.com
-echo "Atlanta, Georgiaa[美东 乔治亚州 亚特兰大]	108.61.193.166" && ping -c 10	ga-us-ping.vultr.com
-echo "Miami, Florida[美东 佛罗里达州 迈阿密]	104.156.244.232" && ping -c 10	fl-us-ping.vultr.com
-echo "Toronto, Canada[加拿大 多伦多]	149.248.50.81" && ping -c 10	tor-ca-ping.vultr.com
+echo "(Asia)Seoul, Korea[韩国 首尔]	141.164.34.61" && ping -c 5	sel-kor-ping.vultr.com
+echo "(Asia)Tokyo, Japan[日本 东京]	108.61.201.151" && ping -c 5	hnd-jp-ping.vultr.com
+echo "Singapore[新加坡]	45.32.100.168" && ping -c 5	sgp-ping.vultr.com
+echo "(AU) Sydney, Australia[悉尼]	108.61.212.117" && ping -c 5	syd-au-ping.vultr.com
+echo "(EU) Frankfurt, DE[德国 法兰克福]	108.61.210.117" && ping -c 5	fra-de-ping.vultr.com
+echo "(EU) Amsterdam, NL[荷兰 阿姆斯特丹]	108.61.198.102" && ping -c 5	ams-nl-ping.vultr.com
+echo "(EU) London, UK[英国 伦敦]	108.61.196.101" && ping -c 5	lon-gb-ping.vultr.com
+echo "(EU) Paris, France[法国 巴黎]	108.61.209.127" && ping -c 5	par-fr-ping.vultr.com
+echo "Seattle, Washington[美东 华盛顿州 西雅图]	108.61.194.105" && ping -c 5	wa-us-ping.vultr.com
+echo "Silicon Valley, Ca[美西 加州 硅谷]	104.156.230.107" && ping -c 5	sjo-ca-us-ping.vultr.com
+echo "Los Angeles, Ca[美西 加州 洛杉矶]	108.61.219.200" && ping -c 5	lax-ca-us-ping.vultr.com
+echo "Chicago, Illinois[美东 芝加哥]	107.191.51.12" && ping -c 5	il-us-ping.vultr.com
+echo "Dallas, Texas[美中 德克萨斯州 达拉斯]	108.61.224.175" && ping -c 5	tx-us-ping.vultr.com
+echo "New York / New Jersey[美东 新泽西]	108.61.149.182" && ping -c 5	nj-us-ping.vultr.com
+echo "Atlanta, Georgiaa[美东 乔治亚州 亚特兰大]	108.61.193.166" && ping -c 5	ga-us-ping.vultr.com
+echo "Miami, Florida[美东 佛罗里达州 迈阿密]	104.156.244.232" && ping -c 5	fl-us-ping.vultr.com
+echo "Toronto, Canada[加拿大 多伦多]	149.248.50.81" && ping -c 5	tor-ca-ping.vultr.com
+
+echo "Stockholm[瑞典斯德哥尔摩]	70.34.194.86" && ping -c 5	sto-se-ping.vultr.com
+echo "Frankfurt[德国法兰福特]	108.61.210.117" && ping -c 5	fra-de-ping.vultr.com
+echo "Madrid	西班牙马德里 208.76.222.30" && ping -c 5 mad-es-ping.vultr.com	  
+echo "Warsaw	波兰华沙 70.34.242.24" && ping -c 5 waw-pl-ping.vultr.com	  
+echo "Mexico City	墨西哥墨西哥城 216.238.66.16" && ping -c 5 mex-mx-ping.vultr.com	  
+echo "Honolulu	美国火奴鲁鲁/檀香山 208.72.154.76" && ping -c 5	hon-hi-us-ping.vultr.com	
+echo "Melbourne	澳大利亚墨尔本 67.219.110.24" && ping -c 5 mel-au-ping.vultr.com	 
+echo "São Paulo	巴西圣保罗 216.238.98.118" && ping -c 5 sao-br-ping.vultr.com	 
